27bdfd4f3cd2dbd47c6a46a4c2261b2af1e63b19
Christopher Parry <parrychri1@ecs.vuw.ac.nz>
2018-08-09 14:58:38 +1200

    Repeat - Started playerOne script #8

--- COMP-MDDNGame/Assets/Scripts/PlayerOneBehaviour.cs
+++ COMP-MDDNGame/Assets/Scripts/PlayerOneBehaviour.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerOneBehaviour : MonoBehaviour {
-
+	Rigidbody2D rb2d;
+	public int speed;
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		float horizontal = Input.GetAxis ("Horizontal");
 	}
 	
 	// Update is called once per frame
