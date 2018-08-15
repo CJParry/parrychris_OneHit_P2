695be3d16502649486dccb9d85d23082e536be76
Christopher Parry <parrychri1@ecs.vuw.ac.nz>
2018-08-15 15:27:46 +1200

    Added horizontal movement for both players with individual keys #8

--- COMP-MDDNGame/Assets/Scripts/PlayerOneBehaviour.cs
+++ COMP-MDDNGame/Assets/Scripts/PlayerOneBehaviour.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerOneBehaviour : MonoBehaviour {
 	Rigidbody2D rb2d;
 	public int speed;
+
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate(){
-		float horizontal = Input.GetAxis ("Horizontal");
+		float horizontal = Input.GetAxis ("HorizontalP1");
+		Vector3 move = new Vector3(horizontal * speed, rb2d.velocity.y, 0f);
+		rb2d.velocity = move;
 	}
 	
 	// Update is called once per frame
